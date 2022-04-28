7d1ed965e129fd4ee0a1eb3e87c0999e9c9d6a85
feittosaa <lucas.feitosa2004@gmail.com>
2022-04-28 15:19:03 -0300

    att 2

--- src/dominio/Menu.java
+++ src/dominio/Menu.java
@@ -5,59 +5,66 @@ import contas.*;
 import java.util.Scanner;
 
 public class Menu {
-	
-	int tipo, login, opcao;
-	double saldo, valor;
-	
+
+	protected int tipo, opcao;
+	protected double saldo, valor;
+	protected String login;
+
 	Scanner ler = new Scanner(System.in);
-	
+
 	public void menu1() {
 		System.out.println("Insira seu cpf");
-		login = ler.nextInt();
-		
+		login = ler.nextLine();
+
 	}
 
-		
-	public double menu2() {
+	public void menu2() {
 		do {
-		System.out.println("Selecione uma opcao abaixo: \n1-Depositar \n2-Sacar \n3-Saldo \n4-Sair");
-		opcao = ler.nextInt();
-		switch(opcao) {
-		
-		case 1:
-			System.out.println("Quanto deseja depositar?");
-			valor = ler.nextDouble();
-			return saldo + valor;
-		case 2:
-			System.out.println("Quanto deseja sacar?");
-			valor = ler.nextDouble();
-			return saldo - valor;
-		case 3:
-			System.out.println(saldo);
-			return saldo;
-		case 4: 
-			System.exit(0);
-		default:
-			return 0;
-		}}while(opcao == 0);
+			System.out.println("Selecione uma opcao abaixo: \n1-Depositar \n2-Sacar \n3-Saldo \n4-Sair");
+			opcao = ler.nextInt();
+			switch (opcao) {
+
+			case 1:
+				System.out.println("Quanto deseja depositar?");
+				valor = ler.nextDouble();
+				System.out.println("Saldo atual: " + (saldo + valor));
+				saldo = saldo + valor;
+				break;
+			case 2:
+				System.out.println("Quanto deseja sacar?");
+				valor = ler.nextDouble();
+				System.out.println("Saldo atual: " + (saldo - valor));
+				saldo = saldo - valor;
+				break;
+			case 3:
+				System.out.println(saldo);
+				break;
+			case 4:
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Selecione uma opcao valida");
+
+			}
+		} while (opcao == 0);
 	}
-	
-	public void tipoDeConta(){
-		
+
+	public void tipoDeConta() {
+
 		System.out.println("Selecione a sua conta \n1 - Poupanca \n2 - Corrente");
 		tipo = ler.nextInt();
-		
-		do{
-			if(tipo == 1) {
-			System.out.println("Conta Poupanca Selecionada");
-			break;
-		}else if(tipo == 2){
-			System.out.println("Conta Corrente Selecionada");
-			break;
-		}else{
-			System.out.println("Selecione 1 ou 2");
-			
-		}
-		}while(true);
+
+		do {
+			if (tipo == 1) {
+				System.out.println("Conta Poupanca Selecionada");
+				break;
+			} else if (tipo == 2) {
+				System.out.println("Conta Corrente Selecionada");
+				break;
+			} else {
+				System.out.println("Selecione 1 ou 2");
+
+			}
+		} while (true);
 	}
 }
--- src/dominio/SistemaInterno.java
+++ src/dominio/SistemaInterno.java
@@ -1,5 +1,7 @@
 package dominio;
 
+import java.util.Scanner;
+
 import contas.*;
 
 import enums.*;
@@ -7,12 +9,27 @@ import enums.*;
 public class SistemaInterno {
 
 	public static void main(String[] args) {
+
+		int continuar;
+
 		Menu m = new Menu();
-		
-		System.out.println();
-		
-		m.menu2();
-		
+		Scanner ler = new Scanner(System.in);
+
+		do {
+			m.menu2();
+			do {
+				System.out.println("Deseja realizar outra operacao? \n1- SIM \n2- SAIR");
+				continuar = ler.nextInt();
+
+				if (continuar == 1) {
+					break;
+				} else if (continuar != 2) {
+					System.out.println("Selecione uma opcao valida");
+					continuar = 1;
+				}
+			} while (continuar == 1);
+
+		} while (continuar == 1);
 	}
 
 }
