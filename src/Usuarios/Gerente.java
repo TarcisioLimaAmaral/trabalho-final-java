f9663863331d6e045c09471c81da9662743ce999
Luiz Felipe <102326788+LuizzFelipe0@users.noreply.github.com>
2022-04-28 18:18:50 -0300

    Update Gerente.java

--- src/Usuarios/Gerente.java
+++ src/Usuarios/Gerente.java
@@ -5,15 +5,9 @@ import enums.CargoFuncionario;
 
 public class Gerente extends Funcionario {
 
-<<<<<<< Updated upstream
-	private AgenciaBanco agencia;
-
-	public Gerente(String cpf, String senha, String nome, AgenciaBanco agenciaG) {
-		super(cpf, senha, nome);
-		this.agencia = agenciaG;
-=======
-	// Puxa a agencia através do gerente no enum AgenciaBanco.
 	private final AgenciaBanco agencia;
+
+	// Puxa a agencia através do gerente no enum AgenciaBanco.
 
 	public AgenciaBanco getAgenciaBanco() {
 		return this.agencia;
@@ -22,7 +16,7 @@ public class Gerente extends Funcionario {
 	public Gerente(String cpf, String senha, String nome, AgenciaBanco agenciaX) {
 		super(cpf, senha, nome);
 		this.agencia = agenciaX;
->>>>>>> Stashed changes
+
 	}
 
 	// Final para puxar o cargo do funcionário Gerente no enum CargoFuncionario.
@@ -32,14 +26,6 @@ public class Gerente extends Funcionario {
 	public static CargoFuncionario getCargoGerente() {
 		return cargoGerente;
 
-<<<<<<< Updated upstream
-	}
 
-	// Get para puxar a Agencia
-	
-	public AgenciaBanco getAgencia() {
-		return agencia;
-=======
->>>>>>> Stashed changes
 	}
 }
